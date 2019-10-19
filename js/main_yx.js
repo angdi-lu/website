a059380a981845fd746c6dc9c6c87f09578e7eb4
Praeter16 <tcosmo1991@gmail.com>
2019-10-19 19:05:10 +0800

    Update main_yx.js

--- js/main_yx.js
+++ js/main_yx.js
@@ -84,6 +84,6 @@ $(document).ready(function() {
 		
 		$(".col1").css("font-size", newFontSize + 'px');
 		$('.col').css("font-size", paraFontSize+ 'px');
-		$('pagetitle').css("font-size",newFontSize+'px')
+		$('sectiontitle').css("font-size",newFontSize+'px')
 	};
 })
