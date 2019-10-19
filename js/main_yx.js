97f84a0ce2b9a7b12ae6654b03e632f3a1e4afdd
Praeter16 <tcosmo1991@gmail.com>
2019-10-19 18:15:22 +0800

    Update main_yx.js

--- js/main_yx.js
+++ js/main_yx.js
@@ -25,3 +25,65 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
+
+$(document).ready(function() {
+	resizeText();
+	var resizeId;
+	$(window).on('resize', function() {
+		clearTimeout(resizeId);
+		resizeId = setTimeout(resizeText, 250);
+		        resizeText();
+	});
+	$(window).on('mousemove', function() {
+		$('*').css('transition-property','');
+	});
+
+	function resizeText() {
+		// Standard height, for which the body font size is correct
+		var preferredFontSize = 16; // %
+
+/*		var preferredSize = 1920 * 1080;
+		var currentSize = $(window).width() * $(window).height();
+		var scalePercentage = Math.sqrt(currentSize) / Math.sqrt(preferredSize);
+		scalePercentage = Math.sqrt(scalePercentage);
+		var newFontSize = preferredFontSize * scalePercentage; */
+
+		var preferredNumber = 70
+		var currentSize = $(window).width()/Math.pow(Math.max(1,0.6*$(window).width()/$(window).height()),0.6)
+		var currentSizeP
+		var currentSizeC
+		var ratio=0.5
+		var fontLargeP=1.5
+		var fontLargeC=1.5
+		if (currentSize >2560 ){
+			currentSize=2560*ratio;
+			currentSizeP=currentSize;
+			currentSizeC=currentSize
+		}
+		else if (currentSize<400){
+			currentSize=currentSize*0.95;
+			currentSizeC=currentSize*fontLargeC;
+			currentSizeP=currentSize*fontLargeP
+		}
+		else {
+			currentSizeP=(ratio+Math.pow((2560-currentSize)/2160,2)*(fontLargeP-ratio))*currentSize;
+			currentSizeC=(ratio+Math.pow((2560-currentSize)/2160,3)*(fontLargeC-ratio))*currentSize;
+			currentSize=(0.95-(0.95-ratio)*(currentSize-400)/2160)*currentSize;
+		}
+		var newFontSize = Math.round(currentSizeC/preferredNumber);
+		var firstHeight = Math.max(50*newFontSize,$(window).height());
+		var paraFontSize = Math.max(Math.round(currentSizeP/preferredNumber),12);
+		var baseLength = 8 * newFontSize;
+		var navHeight = baseLength +0.5*newFontSize;
+		
+		$('*').css('transition-property','none');
+		$('.col').css("width",currentSize+'px');
+		$('.col1').css("min-height",firstHeight+'px');
+		$('pagetitle').css("height",navHeight+'px');
+
+		
+		$(".col1").css("font-size", newFontSize + 'px');
+		$('.col').css("font-size", paraFontSize+ 'px');
+		$('pagetitle').css("font-size",newFontSize+'px')
+	};
+})
