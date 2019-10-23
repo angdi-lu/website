edfb628224cc7c0af1e0a45c6b5d334b16af9a70
Praeter16 <tcosmo1991@gmail.com>
2019-10-23 16:59:14 +0800

    Update main_yx.js

--- js/main_yx.js
+++ js/main_yx.js
@@ -93,9 +93,10 @@ function resizeText() {
         $('.section-left').css("float", 'left')
         $('img').css("margin-left", '-20px')
         $('img').css("max-width", '100%')
+        $('img').css("margin-top", '100px')
         $('aside').css("width", '45%')
         $('aside').css("margin-right", '5%')        
-		$('aside').css("margin-top", '35%')        
+		$('aside').css("margin-top", '30%')        
 
 		$(".col1").css("font-size", newFontSize + 'px');
 		$('.container-alt').css("font-size", paraFontSize+ 'px');
