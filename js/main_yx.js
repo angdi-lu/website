2fe9f7e2fc4baf2dcd5a5d6c18de2e458cde3af9
Praeter16 <tcosmo1991@gmail.com>
2019-10-23 16:52:52 +0800

    Update main_yx.js

--- js/main_yx.js
+++ js/main_yx.js
@@ -98,7 +98,7 @@ function resizeText() {
 		
 		$(".col1").css("font-size", newFontSize + 'px');
 		$('.container-alt').css("font-size", paraFontSize+ 'px');
-		$('.container-profile').css("font-size", '11px');
+		$('.container-profile').css("font-size", '10px');
 		$('.long').css("font-size", paraFontSize+ 'px');
 		$('.short').css("font-size", '14px');
 		$('.biography').css("font-size", '12px');
