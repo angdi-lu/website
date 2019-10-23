8374fb24ed12c5506b06445888e62505c3c3cbce
Praeter16 <tcosmo1991@gmail.com>
2019-10-23 16:18:09 +0800

    Update main_yx.js

--- js/main_yx.js
+++ js/main_yx.js
@@ -89,6 +89,7 @@ function resizeText() {
 		$('.container-profile').css("width", currentSize*0.95+'px');
         
         $('.section-left').css("width", '60%')
+        $('.section-left').css("padding-left", '-30%')
         $('aside').css("margin-right", '10%')
         $('aside').css("width", '45%')
 
